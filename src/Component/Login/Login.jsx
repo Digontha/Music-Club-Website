f35f63eba0bdd3b85983cb08a0bcf97a24a7cd9b
Digontha <digonthodj@gmail.com>
2023-10-07 06:55:10 +0600

    create handlelogin function for user login

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -2,26 +2,32 @@ import { Link } from "react-router-dom";
 
 
 const Login = () => {
+
+  const handleLogin = (e) =>{
+         e.preventDefault();
+         const email = e.target.email.value     
+         const password = e.target.password.value  
+         console.log(email, password);   
+  }
+
+
     return (
-       
-     
-        
         <div className='flex flex-col justify-center items-center mt-10 lg:h-[70vh]'>
             <h1 className="text-2xl font-semibold py-3">Login</h1>
 
               <div className="card w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
+                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
+                <input type="password" placeholder="password" name="password" className="input input-bordered" required />
                 
               </div>
               <div className="form-control mt-6">
